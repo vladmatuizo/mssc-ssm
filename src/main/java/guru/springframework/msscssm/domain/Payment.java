cb601f62b1c43b0a064b62e4b8bf6971abd3bab2
Vladislav Matuizo <dalvmatujzo@gmail.com>
2023-12-18 15:42:01 +0100

    Add domain payment object and repository

---
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -0,0 +1,32 @@
+package guru.springframework.msscssm.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState state;
+
+    private BigDecimal amount;
+}
